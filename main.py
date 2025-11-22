e4eb4db6134c7f8b1793a81317753a6b3378b067
kmaksim2197 <kmaksim2197@gmail.com>
2025-11-22 15:50:53 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+random.randint(100000, 999999)
+n = random.randint(1, 10); fact = 1; 
+for i in range(1, n+1): fact *= i
+random.choice(s)
+n = random.randint(0, 50); n % 7 == 0
+n = random.randint(1, 4); msg = ["bir", "ikki", "uch", "to'rt"][n-1]
